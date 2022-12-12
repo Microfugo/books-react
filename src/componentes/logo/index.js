0d79a8b342e24361c76e62dd3c9c9bd7ac07fa66
Ricardo Rodrigues <ricardobrasfoot@gmail.com>
2022-12-12 19:46:14 -0300

    Usando Map

--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,9 +1,13 @@
 import logo from "../../imagens/logo.svg"
+import "./estilo.css"
 
 function Logo() {
     return (
         <div className="logo">
-            <img src={logo} alt="logo"></img>
+            <img src={logo} 
+            alt="logo"
+            className="logo-img"
+            ></img>
             <p>Books <strong>React</strong></p>
         </div>
     )
