7c4ae3bbf852705e63dcaca8f282d12e8b32df7f
Ricardo Rodrigues <ricardobrasfoot@gmail.com>
2022-12-14 23:29:35 -0300

    Componente OpcoesHeader

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 import './App.css';
-import Logo from "./componentes/Logo"
-import perfil from "./imagens/perfil.svg"
-import sacola from "./imagens/sacola.svg"
+import Logo from './componentes/Logo'
+import perfil from './imagens/perfil.svg'
+import sacola from './imagens/sacola.svg'
+import OpcoesHeader from './componentes/OpcoesHeader'
 
-const textoOpcoes = ["CATEGORIAS", "FAVORITOS", "MINHA ESTANTE"]
 const icones = [perfil, sacola]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Logo></Logo>
-        <ul className="opcoes">
-          {textoOpcoes.map((texto) => (
-            <li className="opcao"> <p>{texto}</p> </li>
-          ))}
-        </ul>
-        
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <Logo></Logo>
+        <OpcoesHeader></OpcoesHeader>
         <ul className='icones'>
-          {icones.map((icone) => (
-            <li className='icone'><img src={icone}></img></li>
-          ))}
+            { icones.map( (icone) => (
+              <li className='icone'><img src={icone}></img></li>
+            )) }
         </ul>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
