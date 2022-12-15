ddded430823fa396a8f80b477c922dfc51a2d2f1
Ricardo Rodrigues <ricardobrasfoot@gmail.com>
2022-12-14 23:41:48 -0300

    Componente Icone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import './App.css';
 import Logo from './componentes/Logo'
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
 import OpcoesHeader from './componentes/OpcoesHeader'
-
-const icones = [perfil, sacola]
+import IconesHeader from './componentes/IconesHeader'
 
 function App() {
     return (
         <div className='App'>
             <header className='App-header'>
                 <Logo></Logo>
-        <OpcoesHeader></OpcoesHeader>
-        <ul className='icones'>
-            { icones.map( (icone) => (
-              <li className='icone'><img src={icone}></img></li>
-            )) }
-        </ul>
-      </header>
-    </div>
+                <OpcoesHeader></OpcoesHeader>
+                <IconesHeader></IconesHeader>
+            </header>
+        </div>
   );
 }
 
