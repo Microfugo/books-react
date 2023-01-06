636accadd4b5c8c8c2f4f9f3b02059286b7b8ff3
Ricardo Rodrigues <ricardobrasfoot@gmail.com>
2023-01-06 00:13:31 -0300

    Props final

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,6 +1,8 @@
 import { livros } from './dadosUltimosLancamentos'
-import styled from 'styled-components'
 import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
+import styled from 'styled-components'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -16,6 +18,8 @@ const NovosLivrosContainer = styled.div`
     justify-content: center;
     cursor: pointer;
 `
+// ...
+
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
@@ -27,9 +31,15 @@ function UltimosLancamentos() {
             </Titulo>
             <NovosLivrosContainer>
                 { livros.map( livro => (
-                    <img src={livro.src} alt ="livros"/>
+                    <img src={livro.src} alt = "livros"/>
                 ) ) }
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
